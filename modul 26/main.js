bef1ca7e080e7d8851f8585dcec57d09b8d4d6d6
edlira2008 <edlirak8@gmail.com>
2026-03-05 17:41:16 +0100

    hehe

--- modul 26/main.js
+++ modul 26/main.js
@@ -1,1 +1,80 @@
 console.log("HELLOOO")
+
+var x = 10;
+var value = "digital school";
+var y = 30;
+
+// kombinime me matematik me x , y 
+console.log(x-y);
+console.log(x/y);
+console.log(x*y);
+console.log(x+y);
+
+// e rrit per ni
+var a = 99;
+a++;
+console.log(a);
+
+
+
+// e zbrit per ni
+var b = 201;
+b--;
+console.log(b);
+
+
+
+
+// mbledhja
+var c = 500;
+b+=5;
+console.log(c);
+
+
+
+
+// zbritja
+var d = 700;
+d-=75;
+console.log(d);
+
+
+
+
+// shumzimi
+var e = 155;
+e*=2;
+console.log(e);
+
+
+
+
+// pjestimi
+var f = 333;
+f/=2;
+console.log(f);
+
+
+
+
+var v = 100;
+var w = 50;
+// operatort e krahasimit
+console.log( v == w);
+console.log( v != w);
+console.log( v > w);
+console.log( v < w);
+
+
+
+//  qato pikat i qesim per me pas hapsir emri
+var emri = "Edlira";
+var mbiemri = "Kastrati";
+var fullName = emri +" "+mbiemri;
+// pa e pas qita sdel qiky kodi nalt
+console.log(fullName);
+
+
+
+//  e tregon ni fjali hello from edlira 
+console.log(`hello from ${emri}`);
