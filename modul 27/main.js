3b225703bca17731fdc9a4ceb29b806db5f87ccd
edlira2008 <edlirak8@gmail.com>
2026-03-10 17:43:09 +0100

    hehe3

---
+++ modul 27/main.js
@@ -0,0 +1,13 @@
+function displayAlert() {
+
+    alert ("this is inside the box");
+}
+displayAlert();
+
+
+function mbledhja (numri1,numri2) {
+
+    return numri1 +numri2 
+} 
+
+document.write (mbledhja(5,8));
