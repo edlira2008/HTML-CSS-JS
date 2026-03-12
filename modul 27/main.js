44cf7144d0f9f06e18312bcc703941f071929419
edlira2008 <edlirak8@gmail.com>
2026-03-12 17:47:02 +0100

    gfhgfh

--- modul 27/main.js
+++ modul 27/main.js
@@ -11,3 +11,60 @@ function mbledhja (numri1,numri2) {
 } 
 
 document.write (mbledhja(5,8));
+
+
+
+
+
+function toCelsius(f)
+
+{
+    return 5/9*(f-32);
+}
+
+
+
+console.log("5 faronhite is equal to "+toCelsius(5)+" celsius");
+
+
+var arrayFunction = () => alert("hello");
+arrayFunction();
+
+function dsFunction() {
+
+    var localvar = "DigitalSchool";
+    alert(localvar);
+
+}
+dsFunction();
+
+
+
+  function toSeconds(m){
+    return  m*60;
+
+  }
+
+console.log(toSeconds(5));
+
+
+  function toParametra(a,b,c) {
+    return  a + b + c;
+  }
+
+console.log(toParametra(5,2,1));
+
+var Car = {name:"Audi" , color:"red", year:2012, vin:"www2342", licensePlate:"01-317-GS"};
+
+
+alert(Car.licensePlate);
+alert(Car['color']);
+
+
+// konstruktori osht si shabllon
+
+function Computer(name , CPU , RAM , GPU) {
+
+
+    th
+}
