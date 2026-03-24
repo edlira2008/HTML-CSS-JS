882194d491e15f0744761c7a6164a188122604a8
edlira2008 <edlirak8@gmail.com>
2026-03-24 17:48:35 +0100

    hfhf

---
+++ modul29/main.js
@@ -0,0 +1,26 @@
+var programingLanguages = ["Python" , "JavaScript" , "Css"];
+console.log(programingLanguages);
+
+
+var programingNumbers = [1,2,3,4,5,6,7,8,9];
+console.log(programingNumbers);
+//  del veq php
+
+console.log(programingLanguages[1]);
+
+
+programingLanguages.push('Java');
+
+programingNummbers.push('11');
+
+
+document.write(programingLanguage);
+document.write(programingNumbers);
+//   pop last element
+
+
+numrat.pop()
+ console.log(numrat);
+
+
+ //unshift - add an element at the begging of the list
