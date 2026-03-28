cb08979b243f785d8206dd924daa4ab0879a19d2
Edlira Kastrati <edlirak8@gmail.com>
2026-03-28 20:31:49 +0100

    jhh

--- moduliushtrimeve/main.js
+++ moduliushtrimeve/main.js
@@ -1,38 +1,38 @@
 
 
- var butoni1 = document.getElementById("btnCircle");
- var butoni2 = document.getElementById("btnTriangle");
- var butoni3 = document.getElementById("btnSquare");
+//  var butoni1 = document.getElementById("btnCircle");
+//  var butoni2 = document.getElementById("btnTriangle");
+//  var butoni3 = document.getElementById("btnSquare");
 
- var div1 = document.getElementById("circle");
- var div2 = document.getElementById("triangle");
- var div3 = document.getElementById("square");
-
-
- butoni1.onclick = function() {
-    div1.setAttribute("class" , "shape_circle");
- }
+//  var div1 = document.getElementById("circle");
+//  var div2 = document.getElementById("triangle");
+//  var div3 = document.getElementById("square");
 
 
-  butoni2.onclick = function() {
-    div2.setAttribute("class" , "shape_triangle");
- }
+//  butoni1.onclick = function() {
+//     div1.setAttribute("class" , "shape_circle");
+//  }
 
 
-  butoni3.onclick = function() {
-    div3.setAttribute("class" , "shape_square");
- }
+//   butoni2.onclick = function() {
+//     div2.setAttribute("class" , "shape_triangle");
+//  }
 
 
-  div1.onclick = function() {
-    div1.setAttribute("class" , "hide");
- }
+//   butoni3.onclick = function() {
+//     div3.setAttribute("class" , "shape_square");
+//  }
 
 
-   div2.onclick = function() {
-    div2.setAttribute("class" , "hide");
- }
+//   div1.onclick = function() {
+//     div1.setAttribute("class" , "hide");
+//  }
 
-   div3.onclick = function() {
-    div3.setAttribute("class" , "hide");
- }
+
+//    div2.onclick = function() {
+//     div2.setAttribute("class" , "hide");
+//  }
+
+//    div3.onclick = function() {
+//     div3.setAttribute("class" , "hide");
+//  }
