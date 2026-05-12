56187f29732101c86322ec01366f007e2970ff79
edlira2008 <edlirak8@gmail.com>
2026-05-12 17:46:12 +0200

    ty

---
+++ modul36/main.js
@@ -0,0 +1,13 @@
+$(window).ready(function(){
+    alert('it is working')
+})
+
+
+$('#button').click(function(){
+    console.log('the testing has bbeen succesfully')
+})
+
+
+$('button').click(function(){
+    $('div').addClass('clicked')
+}).find('span').attr('title','hover over me')
